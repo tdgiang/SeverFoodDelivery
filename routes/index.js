b1586e0175921af7b8aa332e228753a33ea88384
Giang Tran <giangtd@dcv.vn>
2022-07-16 07:09:10 +0700

    Revert "edit login page"
    This reverts commit 789e893c3dbf42604af7c04e9c3beeab6abdd54f.

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var user=require('../models/userModel');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login');
 });
-/* login home page. */
-router.post('/', function(req, res, next) {
-  user.findOne({userName:req.body.email,passWord:req.body.password,author:1},(err,data)=>{
-    if(err){
-      res.redirect('/');
-    }else{
-      if(data){
-        res.redirect('/bills')
-      }else{
-        res.redirect('/');
-         
-      }
-      
-    }
-  })
-
-});
-
 
  
 
